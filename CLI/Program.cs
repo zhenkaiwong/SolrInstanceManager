c219c8e6540075c531f87fa69d0a3c596484dadf
zhenkaiwong <zhenkaiwong.work@gmail.com>
2025-08-25 10:30:55 +0800

    chore: remove unused references

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,6 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SolrInstanceManager.Kernel.Extensions;
